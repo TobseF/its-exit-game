bb9c66250901e10d0eea5139deb23331f7fad8a3
Tobse <Tobse4Git@gmail.com>
2019-08-06 18:42:56 +0200

    🐣 Initial commit with empty LibGDX Setup

---
+++ core/src/main/java/de/tfr/game/ExitGamesStart.java
@@ -0,0 +1,7 @@
+package de.tfr.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class ExitGamesStart extends ApplicationAdapter {
+}
