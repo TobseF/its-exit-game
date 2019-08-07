6c2af14a3b9f8ab155a727d189305698613a85af
Tobse <Tobse4Git@gmail.com>
2019-08-07 15:23:58 +0200

    ➕️ Add `ktx-graphics` module

--- android/src/main/java/de/tfr/game/android/AndroidLauncher.java
+++ android/src/main/java/de/tfr/game/android/AndroidLauncher.java
@@ -1,7 +1,6 @@
 package de.tfr.game.android;
 
 import android.os.Bundle;
-
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import de.tfr.game.ExitGamesStart;
